8c8b3d29d0e288d4889fd3ff7583b4efa7ae02c3
Carlos Umanha <carlosdevu@gmail.com>
2020-04-22 11:54:50 -0500

    All routes created on App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import React, { Component } from 'react'
 import history from './utils/history'
 import { Router, Switch, Route } from 'react-router-dom'
 
+// importing pages
+import { Home } from './pages/Home'
+import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
+import { Dashboard } from './pages/Dashboard'
+import { Devits } from './pages/Devits'
+
 export class App extends Component {
   render () {
     return (
       <Router history={history}>
         <Switch>
-          <Route />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/signin' component={SignIn} />
+          <Route exact path='/signup' component={SignUp} />
+          <Route exact path='/profile' component={Profile} />
+          <Route exact path='/dashboard' component={Dashboard} />
+          <Route exact path='/devits' component={Devits} />
         </Switch>
       </Router>
     )
