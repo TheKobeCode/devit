4e9572b84287c5b717e29fd4e03805877700c658
Carlos Umanha <carlosdevu@gmail.com>
2020-04-22 12:15:08 -0500

    All the firebaseConfig has been updated with the env variables

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ require('dotenv').config()
 
 // firebase config
 var firebaseConfig = {
-  apiKey: 'AIzaSyBdkuV_fPw4G-VU3-4vDicvI7Dre67ekzo',
-  authDomain: 'devit-id.firebaseapp.com',
-  databaseURL: 'https://devit-id.firebaseio.com',
-  projectId: 'devit-id',
-  storageBucket: 'devit-id.appspot.com',
-  messagingSenderId: '301573806125',
-  appId: '1:301573806125:web:0be7e4b2c8a2f5f7f840a4',
-  measurementId: 'G-FKL6DZWLSZ'
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 }
 
 // intializing the app
