610b2c553ee119ec51125df4be8785454fa143b8
Carlos Umanha <carlosdevu@gmail.com>
2020-04-22 11:34:43 -0500

    Theme schema in his first version

---
+++ src/styles/theme.js
@@ -0,0 +1,40 @@
+export const theme = {
+  font: {
+    principalFont: 'Open Sans, sans-serif'
+  },
+
+  colors: {
+    principalColor: '#1A051D',
+    purpleColor: '#3F3356',
+
+    textColor: '#3F3356',
+    lightTextColor: '#D0C9D6',
+
+    linkColor: '#6979F8'
+  },
+
+  textMetrics: {
+    heading1: '34px',
+    heading2: '22px',
+    heading3: '17px',
+    heading4: '15px',
+
+    buttonBig: '17px',
+    buttonNormal: '15px',
+    buttonSmall: '13px',
+
+    caption1: '12px',
+    caption2: '11px',
+
+    body1: '15px',
+    body2: '13px',
+
+    smallText: '11px'
+  },
+
+  fontMetrics: {
+    heading1: '700',
+    link: '600',
+    text: '400'
+  }
+}
