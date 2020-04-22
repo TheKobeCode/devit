41e9f52d6f9e967d05bc2e74e1905cf0cbcc68ad
Carlos Umanha <carlosdevu@gmail.com>
2020-04-22 11:58:50 -0500

    Location configurated for the AnimatePresence component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import history from './utils/history'
 import { Router, Switch, Route } from 'react-router-dom'
+import { AnimatePresence } from 'framer-motion'
 
 // importing pages
 import { Home } from './pages/Home'
@@ -15,14 +16,20 @@ export class App extends Component {
   render () {
     return (
       <Router history={history}>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/signin' component={SignIn} />
-          <Route exact path='/signup' component={SignUp} />
-          <Route exact path='/profile' component={Profile} />
-          <Route exact path='/dashboard' component={Dashboard} />
-          <Route exact path='/devits' component={Devits} />
-        </Switch>
+        <AnimatePresence exitBeforeEnter>
+          <Route
+            render={({ location }) => (
+              <Switch key={location} location={location.pathname}>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/signin' component={SignIn} />
+                <Route exact path='/signup' component={SignUp} />
+                <Route exact path='/profile' component={Profile} />
+                <Route exact path='/dashboard' component={Dashboard} />
+                <Route exact path='/devits' component={Devits} />
+              </Switch>
+            )}
+          />
+        </AnimatePresence>
       </Router>
     )
   }
