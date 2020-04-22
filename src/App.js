c3d2713e365d2d377f5b931e04e8d81243c369c4
Carlos Umanha <carlosdevu@gmail.com>
2020-04-22 13:40:14 -0500

    Initial state for the entire app setted on the AuthContext

--- src/App.js
+++ src/App.js
@@ -11,26 +11,29 @@ import { SignUp } from './pages/SignUp'
 import { Profile } from './pages/Profile'
 import { Dashboard } from './pages/Dashboard'
 import { Devits } from './pages/Devits'
+import { AuthContextProvider } from './contexts/AuthContext'
 
 export class App extends Component {
   render () {
     return (
-      <Router history={history}>
-        <AnimatePresence exitBeforeEnter>
-          <Route
-            render={({ location }) => (
-              <Switch key={location.pathname} location={location}>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/signin' component={SignIn} />
-                <Route exact path='/signup' component={SignUp} />
-                <Route exact path='/profile' component={Profile} />
-                <Route exact path='/dashboard' component={Dashboard} />
-                <Route exact path='/devits' component={Devits} />
-              </Switch>
-            )}
-          />
-        </AnimatePresence>
-      </Router>
+      <AuthContextProvider>
+        <Router history={history}>
+          <AnimatePresence exitBeforeEnter>
+            <Route
+              render={({ location }) => (
+                <Switch key={location.pathname} location={location}>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/signin' component={SignIn} />
+                  <Route exact path='/signup' component={SignUp} />
+                  <Route exact path='/profile' component={Profile} />
+                  <Route exact path='/dashboard' component={Dashboard} />
+                  <Route exact path='/devits' component={Devits} />
+                </Switch>
+              )}
+            />
+          </AnimatePresence>
+        </Router>
+      </AuthContextProvider>
     )
   }
 }
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,8 @@
 import React, { Component, createContext } from 'react'
 
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
 export const AuthContext = createContext()
 
 export class AuthContextProvider extends Component {
@@ -7,13 +10,49 @@ export class AuthContextProvider extends Component {
     super(props)
 
     this.state = {
-      isAuth: false
+      isAuth: null,
+      loading: false
     }
+  }
+
+  // function to set Auth state of the entire application
+  getAuth = () => {
+    this.setState({
+      isAuth: false,
+      loading: true
+    })
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          loading: false,
+          isAuth: true
+        })
+      } else {
+        this.setState({
+          loading: false,
+          isAuth: false
+        })
+      }
+    })
+  }
+
+  // this function can be used on the consumer
+  // toggle the state of isAuth
+  toggleAuth = () => {
+    this.setState({
+      isAuth: !this.state.isAuth
+    })
+  }
+
+  componentDidMount = () => {
+    this.getAuth()
   }
 
   render () {
     return (
-      <AuthContext.Provider value={{ ...this.state }}>
+      <AuthContext.Provider
+        value={{ ...this.state, toggleAuth: this.toggleAuth }}
+      >
         {this.props.children}
       </AuthContext.Provider>
     )
