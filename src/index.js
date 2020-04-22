4833f0a92d363db7ca064addfbfa7923ce97b0c6
Carlos Umanha <carlosdevu@gmail.com>
2020-04-22 12:41:23 -0500

    Bug fixed but im not sure how is it good

--- src/index.js
+++ src/index.js
@@ -4,19 +4,7 @@ import { App } from './App'
 
 import firebase from 'firebase/app'
 import 'firebase/analytics'
-
-require('dotenv').config()
-
-var firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID
-}
+import { firebaseConfig } from './utils/firebaseConfig'
 
 // intializing the app
 firebase.initializeApp(firebaseConfig)
---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,10 @@
+export var firebaseConfig = {
+  apiKey: 'AIzaSyBdkuV_fPw4G-VU3-4vDicvI7Dre67ekzo',
+  authDomain: 'devit-id.firebaseapp.com',
+  databaseURL: 'https://devit-id.firebaseio.com',
+  projectId: 'devit-id',
+  storageBucket: 'devit-id.appspot.com',
+  messagingSenderId: '301573806125',
+  appId: '1:301573806125:web:0be7e4b2c8a2f5f7f840a4',
+  measurementId: 'G-FKL6DZWLSZ'
+}
